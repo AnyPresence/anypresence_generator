5a2a38bf0e8e9e4822152b01663394fb62feb7cd
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-04-24 18:35:22 -0400

    Cleanup by moving command into module.

--- lib/anypresence_generator/command.rb
+++ lib/anypresence_generator/command.rb
@@ -1,36 +1,41 @@
+require 'bundler'
+
 module AnypresenceGenerator
   module Command
-    include ::AnypresenceGenerator::Log
-    def run_command(command_string, options={})
-      abort = options[:abort].nil? ? true : options[:abort]
+
+    attr_accessor :sensitive_values
+
+    def run_command(command_string, abort: true, silence: false, filter: nil)
 
       filtered_command = command_string.clone
-      @sensitive_values ||= {}
-      @sensitive_values.each do |sensitive, filtered|
+      sensitive_values.each do |sensitive, filtered|
         filtered_command.gsub! sensitive, filtered
       end
 
-      log filtered_command, :timestamp => false unless options[:silence]
-      @last_output = `#{command_string} 2>&1`
+      log filtered_command, timestamp: false unless silence
+      last_output = nil
+      Bundler.with_clean_env do
+        last_output = `#{command_string} 2>&1`
+      end
       filtered_output = nil
-      if !options[:silence] && options[:filter] && options[:filter].is_a?(Regexp)
-        res = @last_output.each_line.select { |line| line if line =~ options[:filter] }
-        filtered_output = res.join("\n")
+      if !silence && filter && filter.is_a?(Regexp)
+        result = last_output.each_line.select { |line| line if line =~ filter }
+        filtered_output = result.join("\n")
       end
 
-      if !options[:silence]
-        output = filtered_output || @last_output
-        log_output output
+      unless silence
+        output = filtered_output || last_output
+        log output
       end
 
       if 0 != $?.to_i
         if abort
           raise ::AnypresenceGenerator::Workhorse::WorkableError.new("command failed with exit code #{$?}")
-        elsif !options[:silence]
+        elsif !silence
           log "#{$?}, aborting"
         end
       else
-        @last_output
+        last_output
       end
     end
   end
