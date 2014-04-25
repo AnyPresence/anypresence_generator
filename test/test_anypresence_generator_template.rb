c8fc8664eb4e20289587e02e7243be63abeb674b
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-04-25 13:27:58 -0400

    Tests for templates.

---
+++ test/test_anypresence_generator_template.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require 'support/magical_generator'
+require 'support/magical_template'
+
+class AnypresenceGeneratorTemplateTest < Test::Unit::TestCase
+
+  def test_anypresence_generator_template_subclasses
+    json = File.read(File.join(File.expand_path(File.dirname(__FILE__)), "support/api.txt"))
+    assert_nothing_raised do
+      generator = MagicalGeneratorWithTemplate.new(json_payload: json, auth_token: 'test', mock: true)
+      MagicalTemplate.new(generator: generator)
+    end
+  end
+
+  def test_anypresence_generator_with_templates
+    json = File.read(File.join(File.expand_path(File.dirname(__FILE__)), "support/api.txt"))
+    Dir.mktmpdir do |dir|
+      generator = MagicalGeneratorWithTemplate.new(json_payload: json, auth_token: 'test', mock: true, dump_project_directory: dir)
+      assert generator.start!
+      template_output = File.read(File.expand_path('magic.txt', dir))
+      generator.object_definitions.each do |object_defintion|
+        assert template_output.include?(object_defintion.name)
+        object_defintion.field_definitions.each { |field_definition| assert template_output.include?(field_definition.name) }
+      end
+    end
+  end
+
+end
