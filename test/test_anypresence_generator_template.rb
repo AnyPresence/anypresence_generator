f9d0023d969fd5163e53e9ff158466562608d7a0
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-05-01 15:22:21 -0400

    New test for templates with explicit output files and paths.

--- test/test_anypresence_generator_template.rb
+++ test/test_anypresence_generator_template.rb
@@ -25,4 +25,17 @@ class AnypresenceGeneratorTemplateTest < Test::Unit::TestCase
     end
   end
 
+  def test_anypresence_generator_with_templates_and_output_file
+    json = File.read(File.join(File.expand_path(File.dirname(__FILE__)), "support/api.txt"))
+    Dir.mktmpdir do |dir|
+      generator = MagicalGeneratorWithTemplateAndOutput.new(json_payload: json, auth_token: 'test', mock: true, dump_project_directory: dir)
+      assert generator.start!
+      template_output = File.read(File.join(dir, MagicalTemplateWithSpecificOutput.new(generator: generator).output_path, MagicalTemplateWithSpecificOutput.new(generator: generator).output_file))
+      generator.object_definitions.each do |object_defintion|
+        assert template_output.include?(object_defintion.name)
+        object_defintion.field_definitions.each { |field_definition| assert template_output.include?(field_definition.name) }
+      end
+    end
+  end
+
 end
