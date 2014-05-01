ac76790868a8faa7c37636d4d5afe3eee269910d
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-05-01 15:21:07 -0400

    Templates now allow explicit setting of output file and output path. If no output file is set, template file name sans .erb is assumed. If not output path is set, the root of the project directory is assumed.

--- lib/anypresence_generator/template.rb
+++ lib/anypresence_generator/template.rb
@@ -12,33 +12,45 @@ module AnypresenceGenerator
     def initialize(generator: ( raise TemplateError.new('Generator workhorse is required.'.freeze) ) )
       self.generator = generator
       self.project_directory = generator.project_directory
+      raise TemplateError.new('template_file must be set'.freeze) unless respond_to?(:template_file)
       raise TemplateError.new('templates_path must be set'.freeze) unless respond_to?(:templates_path)
       raise TemplateError.new('partials_path must be set'.freeze) unless respond_to?(:partials_path)
     end
 
     # Writes the generated file(s) to disk in the directory passed.
     def write!
-      path = File.join(project_directory, filename).split('/').reverse.drop(1).reverse.join('/')
+      path = File.join(project_directory, output_path)
       FileUtils.mkdir_p(path) unless File.exists?(path)
 
-      File.open(File.join(project_directory, filename), 'w'.freeze) do |file|
+      File.open(File.join(path, output_file), 'w'.freeze) do |file|
         proc = ::Proc.new {}
-        file.write ERB.new(File.read(template_file),nil,'<>'.freeze).result(proc.binding)
+        file.write ERB.new(File.read(File.join(templates_path, "#{template_file}.erb")), nil, '<>'.freeze).result(proc.binding)
       end
     end
 
-    # Returns the absolute path of this instance's source template file
-    def template_file
-      File.expand_path("#{filename.split('/').last}.erb", templates_path)
-    end
-
-    # Returns the absolute path of the source partial template with the given +partial_name+   
+    # Returns the absolute path of the source partial template with the given +partial_name+
     def partial_file(name: ( raise TemplateError.new('Partial name is required.'.freeze) ))
       File.expand_path("_#{name}.erb", partials_path)
     end
 
-    def self.file(filename)
-      define_method(:filename) { filename }
+    def self.template_file(filename)
+      define_method(:template_file) { filename }
+    end
+
+    def self.output_file(filename)
+      define_method(:output_file) { filename }
+    end
+
+    def output_file
+      template_file
+    end
+
+    def self.output_path(path)
+      define_method(:output_path) { path }
+    end
+
+    def output_path
+      '/'.freeze
     end
 
     def self.templates_path(template_path)
