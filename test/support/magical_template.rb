9d83564ea25095bd0b33bc6f2b56ca18adb0bc26
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-05-01 15:22:03 -0400

    New template class for tests that specifies output file and path.

--- test/support/magical_template.rb
+++ test/support/magical_template.rb
@@ -1,7 +1,15 @@
 require 'anypresence_generator'
 
 class MagicalTemplate < AnypresenceGenerator::Template
-  file 'magic.txt'
+  template_file 'magic.txt'
+  templates_path File.dirname(__FILE__)
+  partials_path File.expand_path("partials", File.dirname(__FILE__))
+end
+
+class MagicalTemplateWithSpecificOutput < AnypresenceGenerator::Template
+  template_file 'magic.txt'
+  output_file 'unicorn.rb'
+  output_path '/magical/path'
   templates_path File.dirname(__FILE__)
   partials_path File.expand_path("partials", File.dirname(__FILE__))
 end
