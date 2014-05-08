da46f2046641a2152b0b36020cd1a5b194081fac
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-05-08 12:27:56 -0400

    New generator workhorse subclass.

---
+++ lib/anypresence_generator/generator.rb
@@ -0,0 +1,12 @@
+require 'anypresence_generator/workhorse'
+require 'anypresence_generator/payload/build'
+
+module AnypresenceGenerator
+  class Generator < AnypresenceGenerator::Workhorse
+    include AnypresenceGenerator::Payload::Build
+
+    def steps
+      [:setup_repository, :init_or_clone] + self.class._steps + [:commit_to_repository, :push_to_repository, :upload_artifacts]
+    end
+  end
+end
