5889f15f79eb3e010f2b2d7c968762e9ead881bc
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-06-25 16:03:36 -0400

    Always use IO.popen and always streaming command results to log.

--- lib/anypresence_generator/command.rb
+++ lib/anypresence_generator/command.rb
@@ -5,7 +5,7 @@ module AnypresenceGenerator
 
     attr_accessor :sensitive_values
 
-    def run_command(command_string, abort: true, silence: false, filter: nil, streaming: true)
+    def run_command(command_string, abort: true, silence: false, filter: nil)
       filtered_command = command_string.clone
       sensitive_values.each do |sensitive, filtered|
         filtered_command.gsub! sensitive, filtered
@@ -15,40 +15,14 @@ module AnypresenceGenerator
 
       command_output = ""
       Bundler.with_clean_env do
-        if streaming
-          IO.popen("#{command_string} 2>&1") do |io|
-            until io.eof?
-              buffer = io.gets
-              command_output << handle_command_output(buffer, abort, silence, filter)
-            end
+        IO.popen("#{command_string} 2>&1") do |io|
+          until io.eof?
+            buffer = io.gets
+            command_output << handle_command_output(buffer, abort, silence, filter)
           end
-          output_or_abort("", abort, silence)
-        else
-          last_output = `#{command_string} 2>&1`
-          command_output << handle_command_output(last_output, abort, silence, filter)
+          io.close
         end
       end
-      command_output
-    end
-    
-    private 
-    
-    def handle_command_output(last_output, abort, silence, filter)
-      filtered_output = nil
-      if !silence && filter && filter.is_a?(Regexp)
-        result = last_output.each_line.select { |line| line if line =~ filter }
-        filtered_output = result.join("\n")
-      end
-
-      unless silence
-        output = filtered_output || last_output
-        log output
-      end
-      
-      output_or_abort(last_output, abort, silence)
-    end
-    
-    def output_or_abort(output, abort, silence)
       if 0 != $?.exitstatus
         if abort
           raise ::AnypresenceGenerator::Workhorse::WorkableError.new("command failed with exit code #{$?.exitstatus}")
@@ -56,9 +30,23 @@ module AnypresenceGenerator
           log "#{$?}, aborting"
         end
       else
-        output
+        command_output
       end
     end
-    
+
+    private
+
+    def handle_command_output(last_output, abort, silence, filter)
+      filtered_output = nil
+      if !silence && filter && filter.is_a?(Regexp)
+        result = last_output.each_line.select { |line| line if line =~ filter }
+        filtered_output = result.join("\n")
+      end
+
+      output = filtered_output || last_output
+      log output unless silence
+      output
+    end
+
   end
 end
