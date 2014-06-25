699ca1d616ffc90ab216ddf728d0bf9d8b0b9587
Nass <nasali@anypresence.com>
2014-06-25 14:50:02 -0400

    Implement streaming log support.

--- lib/anypresence_generator/command.rb
+++ lib/anypresence_generator/command.rb
@@ -5,18 +5,35 @@ module AnypresenceGenerator
 
     attr_accessor :sensitive_values
 
-    def run_command(command_string, abort: true, silence: false, filter: nil)
-
+    def run_command(command_string, abort: true, silence: false, filter: nil, streaming: true)
       filtered_command = command_string.clone
       sensitive_values.each do |sensitive, filtered|
         filtered_command.gsub! sensitive, filtered
       end
 
       log filtered_command unless silence
-      last_output = nil
+
+      command_output = ""
       Bundler.with_clean_env do
-        last_output = `#{command_string} 2>&1`
+        if streaming
+          IO.popen("#{command_string} 2>&1") do |io|
+            until io.eof?
+              buffer = io.gets
+              command_output << handle_command_output(buffer, abort, silence, filter)
+            end
+          end
+          output_or_abort("", abort, silence)
+        else
+          last_output = `#{command_string} 2>&1`
+          command_output << handle_command_output(last_output, abort, silence, filter)
+        end
       end
+      command_output
+    end
+    
+    private 
+    
+    def handle_command_output(last_output, abort, silence, filter)
       filtered_output = nil
       if !silence && filter && filter.is_a?(Regexp)
         result = last_output.each_line.select { |line| line if line =~ filter }
@@ -27,16 +44,21 @@ module AnypresenceGenerator
         output = filtered_output || last_output
         log output
       end
-
-      if 0 != $?.to_i
+      
+      output_or_abort(last_output, abort, silence)
+    end
+    
+    def output_or_abort(output, abort, silence)
+      if 0 != $?.exitstatus
         if abort
-          raise ::AnypresenceGenerator::Workhorse::WorkableError.new("command failed with exit code #{$?}")
+          raise ::AnypresenceGenerator::Workhorse::WorkableError.new("command failed with exit code #{$?.exitstatus}")
         elsif !silence
           log "#{$?}, aborting"
         end
       else
-        last_output
+        output
       end
     end
+    
   end
 end
---
+++ test/test_anypresence_command.rb
@@ -0,0 +1,36 @@
+require 'test/unit'
+require 'support/magical_generator'
+
+class AnypresenceGeneratorCommandTest < Test::Unit::TestCase
+  
+  def setup
+    json = File.read(File.join(File.expand_path(File.dirname(__FILE__)), "support/api.txt"))
+    @generator = MagicalGenerator.new(json_payload: json, auth_token: 'test', mock: true)
+  end
+  
+  def test_anypresence_command
+    puts "\nExisting status code is #{$?.exitstatus}"
+    assert_nothing_raised do
+      @generator.run_command('date')
+      @generator.run_command('date', abort: false)
+      @generator.run_command('date', streaming: false)
+    end
+    puts "\nAFTER running status code is #{$?.exitstatus}"
+  end
+  
+  def test_anypresence_command_silence
+    puts "\nExisting status code is #{$?.exitstatus}"
+    assert @generator.log_content.strip.empty?
+    @generator.run_command('date', silence: true)      
+    assert @generator.log_content.strip.empty?
+    puts "\nAFTER running status code is #{$?.exitstatus}"
+  end
+  
+  def test_anypresence_command_should_abort
+    puts "\nExisting status code is #{$?.exitstatus}"
+    assert_raise ::AnypresenceGenerator::Workhorse::WorkableError do
+      @generator.run_command('magical_command_that_does_not_exist')
+    end
+    puts "\nAFTER running status code is #{$?.exitstatus}"
+  end
+end
