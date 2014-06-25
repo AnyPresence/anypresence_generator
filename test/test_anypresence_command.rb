323b663b0f493ff03760cd9267e281fcae00ecb0
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-06-25 16:17:56 -0400

    Remove puts calls from tests.

--- test/test_anypresence_command.rb
+++ test/test_anypresence_command.rb
@@ -9,28 +9,22 @@ class AnypresenceGeneratorCommandTest < Test::Unit::TestCase
   end
 
   def test_anypresence_command
-    puts "\nExisting status code is #{$?.exitstatus}"
     assert_nothing_raised do
       @generator.run_command('date')
       @generator.run_command('date', abort: false)
       @generator.run_command('date')
     end
-    puts "\nAFTER running status code is #{$?.exitstatus}"
   end
 
   def test_anypresence_command_silence
-    puts "\nExisting status code is #{$?.exitstatus}"
     assert @generator.log_content.strip.empty?
-    @generator.run_command('date', silence: true)      
+    @generator.run_command('date', silence: true)
     assert @generator.log_content.strip.empty?
-    puts "\nAFTER running status code is #{$?.exitstatus}"
   end
 
   def test_anypresence_command_should_abort
-    puts "\nExisting status code is #{$?.exitstatus}"
     assert_raise ::AnypresenceGenerator::Workhorse::WorkableError do
       @generator.run_command('magical_command_that_does_not_exist')
     end
-    puts "\nAFTER running status code is #{$?.exitstatus}"
   end
 end
