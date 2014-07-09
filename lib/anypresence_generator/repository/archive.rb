6017ed0b9e5f9334e5c634007ff4f229c83e3cdc
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-07-09 18:33:15 -0400

    Pass workhorse the git archive. Execute system calls through run_command.

--- lib/anypresence_generator/repository/archive.rb
+++ lib/anypresence_generator/repository/archive.rb
@@ -7,7 +7,7 @@ module AnypresenceGenerator
 
       attr_accessor :readable_git_url, :writeable_git_url
 
-      def initialize( repository_payload: repository_payload, directory: ( raise GitError.new("Directory is required.") ), mock: false )
+      def initialize( workhorse: , repository_payload: repository_payload, directory: ( raise GitError.new("Directory is required.") ), mock: false )
         self.readable_git_url = repository_payload.readable_url
         self.writeable_git_url = repository_payload.writeable_url
         super
@@ -24,20 +24,12 @@ module AnypresenceGenerator
             archive_file.write(remote_file.read)
           end
         end
-        last_output = %x|tar -xf "#{File.path(git_archive)}" -C "#{directory}" 2>&1|
-
-        unless $?.success?
-          raise ::AnypresenceGenerator::Repository::Git::GitError.new("Git archive clone failed with exit code: #{$?}\n#{last_output}")
-        end
+        workhorse.run_command(%|tar -xf "#{File.path(git_archive)}" -C "#{directory}"|)
 
         if recursive
           Bundler.with_clean_env do
             Dir.chdir(@git.dir.to_s) do
-              last_output = %x|git submodule update --init --recursive 2>&1|
-
-              unless $?.success?
-                raise ::AnypresenceGenerator::Repository::Git::GitError.new("Git archive submodule update failed with exit code: #{$?}\n#{last_output}")
-              end
+              workhorse.run_command(%|git submodule update --init --recursive|)
             end
           end
         end
@@ -53,12 +45,9 @@ module AnypresenceGenerator
             gitignore = File.read("#{directory}/.gitignore")
             exclude = ""
             gitignore.split("\n").each { |ignore| exclude << %|--exclude="./#{ignore.start_with?('/') ? ignore.sub('/','') : ignore}" | if !ignore.start_with?('#') && !ignore.nil? && !ignore.blank? }
-            last_output = %x|tar -cvzf "#{archive_source.path}" -C "#{directory}" #{exclude} "." 2>&1|
+            workhorse.run_command(%|tar -cvzf "#{archive_source.path}" -C "#{directory}" #{exclude} "."|)
           else
-            last_output = %x|tar -cvzf "#{archive_source.path}" -C "#{directory}" "." 2>&1|
-          end
-          unless $?.success?
-            raise ::AnypresenceGenerator::Repository::Git::GitError.new("Git archiving failed with exit code: #{$?}\n#{last_output}")
+            workhorse.run_command(%|tar -cvzf "#{archive_source.path}" -C "#{directory}" "."|)
           end
           RestClient.put( writeable_git_url, File.open(archive_source), multipart: true, content_type: 'application/zip' ) unless mock
           FileUtils.cp(File.path(archive_source), "#{directory}/git_archive.zip")
