76f3b2a00ff32516dc397323ea0642948f4909da
Jeffrey Bozek <jeffreybozek@jbmbpr.home>
2014-07-16 19:22:49 -0400

    Use git directly via run_command

--- lib/anypresence_generator/repository/git.rb
+++ lib/anypresence_generator/repository/git.rb
@@ -1,4 +1,3 @@
-require 'git'
 require 'bundler'
 
 module AnypresenceGenerator
@@ -6,7 +5,7 @@ module AnypresenceGenerator
     class Git
       class GitError < StandardError; end
 
-      attr_accessor :workhorse, :git, :git_url, :directory, :pushed, :mock
+      attr_accessor :workhorse, :git_url, :directory, :pushed, :mock
 
       def initialize( workhorse: , repository_payload: repository_payload, directory: ( raise GitError.new("Directory is required.") ), mock: false )
         self.workhorse = workhorse
@@ -17,36 +16,36 @@ module AnypresenceGenerator
       end
 
       def init
-        self.git = ::Git.init(directory)
+        workhorse.run_command(%|cd #{directory} && git init|)
+        workhorse.run_command(%|cd #{directory} && git remote add origin #{git_url}|) if git_url
       end
 
       def clone(recursive: false)
-        self.git = ::Git.clone(git_url, '', path: directory)
-
-        workhorse.run_command("cd #{git.dir} && git submodule update --init --recursive") if recursive
+        workhorse.run_command(%|cd #{directory} && git clone #{git_url} .|)
+        workhorse.run_command(%|cd #{directory} && git submodule update --init --recursive|) if recursive
       end
 
       def commit(user_name: nil, user_email: nil, commit_message: nil)
-        git.config('user.name', user_name)
-        git.config('user.email', user_email)
-        git.add(all: true)
-        git.commit_all commit_message
+        workhorse.run_command(%|cd #{directory} && git config --local user.name "#{user_name}"|)
+        workhorse.run_command(%|cd #{directory} && git config --local user.email "#{user_email}"|)
+        workhorse.run_command(%|cd #{directory} && git add --all -- "."|)
+        workhorse.run_command(%|cd #{directory} && git commit -m "#{commit_message}"|)
       end
 
-      def push(remote: nil)
+      def push(remote_name: nil, remote_url: nil)
         unless mock
-          if remote
-            git.add_remote(remote, git_url)
-            git.push(git.remote(remote))
+          if remote_name
+            run_command(%|cd #{directory} && git remote add #{remote_name} #{remote_url}|)
+            run_command(%|cd #{directory} && git push #{remote_name} master|)
           else
-            git.push git_url
+            run_command(%|cd #{directory} && git push origin master|)
           end
         end
       end
 
       def add_submodule(local: nil, remote: nil, branch: 'master')
-        workhorse.run_command(%|cd #{git.dir} && git submodule deinit --force "#{local}" && git rm --force "#{local}" && git config -f .gitmodules --remove-section submodule."#{local}" && rm -Rf .git/modules/#{local}|, abort: false)
-        workhorse.run_command(%|cd #{git.dir} && git submodule add -b "#{branch}" --force "#{remote}" "#{local}"|)
+        workhorse.run_command(%|cd #{directory} && git submodule deinit --force "#{local}" && git rm --force "#{local}" && git config -f .gitmodules --remove-section submodule."#{local}" && rm -Rf .git/modules/#{local}|, abort: false)
+        workhorse.run_command(%|cd #{directory} && git submodule add -b "#{branch}" --force "#{remote}" "#{local}"|)
       end
     end
   end
